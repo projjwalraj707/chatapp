38d76043e8e6d0466fb77e599da3077d8bf4c5e9
Projjwal Raj <piyushraj707@gmail.com>
2026-05-05 00:30:12 +0530

    .

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -13,37 +13,37 @@ export default function Signup() {
 				<div className='divider'></div>
 				<form action={actionFunc}>
 					<label>Name
-						{state?.errors.name && (<span className='text-red-500 bold'>: {state.errors.name[0]}</span>)}
+						{(state?.errors as any)?.name && (<span className='text-red-500 bold'>: {(state?.errors as any).name[0]}</span>)}
 						<br />
 						<input name='name' type='text' defaultValue={state?.formData.name as string} placeholder='Enter your Full Name' /> <br />
 					</label>
 
 					<label>Email
-						{state?.errors.email && (<span className='text-red-500 bold'>: {state.errors.email[0]}</span>)}
+						{(state?.errors as any)?.email && (<span className='text-red-500 bold'>: {(state?.errors as any).email[0]}</span>)}
 						<br />
 						<input name='email' type='email' defaultValue={state?.formData.email as string} placeholder='Enter your Email' /> <br />
 					</label>
 
 					<label>Username
-						{state?.errors.username && (<span className='text-red-500 bold'>: {state.errors.username[0]}</span>)}
+						{(state?.errors as any)?.username && (<span className='text-red-500 bold'>: {(state?.errors as any).username[0]}</span>)}
 						<br />
 						<input name='username' type='text' defaultValue={state?.formData.username as string} placeholder='Enter your Username' /> <br />
 					</label>
 
 					<label>Password
-						{state?.errors.password && (<span className='text-red-500 bold'>: {state.errors.password[0]}</span>)}
+						{(state?.errors as any)?.password && (<span className='text-red-500 bold'>: {(state?.errors as any).password[0]}</span>)}
 						<br />
 						<input name='password' type='password' defaultValue={state?.formData.password as string} placeholder='Enter Your Password' /> <br />
 					</label>
 
 					<label>Confirm Password
-						{state?.errors.confirmPassword && (<span className='text-red-500 bold'>: {state.errors.confirmPassword[0]}</span>)}
+						{(state?.errors as any)?.confirmPassword && (<span className='text-red-500 bold'>: {(state?.errors as any).confirmPassword[0]}</span>)}
 						<br />
 						<input name='confirmPassword' type='password' defaultValue={state?.formData.confirmPassword as string} placeholder='Confirm your Password' /> <br />
 					</label>
 
 					<div className='divider'></div>
-					{state?.errors.generalError && (<span className='text-red-500 bold'>{state.errors.generalError[0]}</span>)}
+					{(state?.errors as any)?.generalError && (<span className='text-red-500 bold'>{(state?.errors as any).generalError[0]}</span>)}
 					<div className='button-container'>
 						<button className={`submit-button ${isPending ? "button-disabled" : "button-abled"}`} type='submit' disabled={isPending}>
 							{isPending ? "Submitting..." : "Submit"}
